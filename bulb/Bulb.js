5a060582f0f89b90769ad61cac9c8e57a068ed77
Vaishak T <62879169+vaisx05@users.noreply.github.com>
2023-07-31 17:03:24 +0530

    Add files via upload

---
+++ bulb/Bulb.js
@@ -0,0 +1,7 @@
+function on(){
+    document.getElementById("1").src="https://i.postimg.cc/6QyTynzr/bulb-on.png";
+}
+
+function off(){
+    document.getElementById("2").src="https://i.postimg.cc/KjK1wL3c/bulb-off.png";
+}
